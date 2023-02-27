41dd24aa0a9d05d44496d0e12c81578632b4c682
araceli <asoffulto@frba.utn.edu.ar>
2023-02-27 00:16:47 -0300

    ultimo, mejora del py

--- formatter_1.py
+++ formatter_1.py
@@ -2,31 +2,41 @@
 import json
 import copy
 
+import sys
+
+print('cmd entry:', sys.argv)
+
 # the file to be converted to
 # json format
-filename = 'data.txt'
-
-# dictionary where the lines from
-# text will be stored
+filename = sys.argv[1]
+name = sys.argv[2]
 list = []
-pregunta = "pregunta"
-resp = "respuesta"
 i=0
 d = {}
 # creating dictionary
 with open(filename) as fh:   
     for line in fh:
-        if(i%2 == 0 or i == 0):
-            d[pregunta] = line.strip()
-        else:
-            d[resp]= line.strip()
+        if(line.upper().strip() == 'TEORIA' or line.upper().strip() == 'PRACTICA'):
+            d['esSeparador']= True
+            d['texto']= line.strip()
             list.append(copy.deepcopy(d))
+            d = {}
+            i =-1
+        elif(i%2 == 0 or i == 0):
+            d['pregunta'] = line.strip()
+        else:
+            d['respuesta']= line.strip()
+            list.append(copy.deepcopy(d))
+            d = {}
         i=i+1
-        
-print(list)
+    
+final = {
+    "nombre": name,
+    "preguntas": list
+}
 
 # creating json file
 # the JSON file is named as test1
 out_file = open("test1.json", "w")
-json.dump(list, out_file, indent = 4, sort_keys = False, ensure_ascii=False)
+json.dump(final, out_file, indent = 4, sort_keys = False, ensure_ascii=False)
 out_file.close()
